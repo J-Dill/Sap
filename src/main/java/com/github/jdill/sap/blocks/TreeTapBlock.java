78dfb36a991f1c1ab3647e14be3353e7ef2fd2be
Dylan <dylan.jech@mnsu.edu>
2021-02-06 15:19:47 -0600

    Places on back you click on and added better model

--- src/main/java/com/github/jdill/sap/blocks/TreeTapBlock.java
+++ src/main/java/com/github/jdill/sap/blocks/TreeTapBlock.java
@@ -2,26 +2,76 @@ package com.github.jdill.sap.blocks;
 
 import com.github.jdill.sap.Registry;
 import com.github.jdill.sap.tileentity.TreeTapTileEntity;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class TreeTapBlock extends Block {
 
     public static final String ID = "tree_tap";
     private static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.makeCuboidShape(7, 6.6, 10, 9, 7.6, 16),
+            Block.makeCuboidShape(7, 5.6, 9, 9, 6.6, 10),
+            Block.makeCuboidShape(7, 7.6, 12, 9, 9.6, 14),
+            Block.makeCuboidShape(6, 8.6, 12, 10, 9.6, 14),
+            Block.makeCuboidShape(7, 8.6, 11, 9, 9.6, 15)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+
+    private static final VoxelShape SHAPE_E = Stream.of(
+            Block.makeCuboidShape(0, 6.6, 7, 6, 7.6, 9),
+            Block.makeCuboidShape(6, 5.6, 7, 7, 6.6, 9),
+            Block.makeCuboidShape(2, 7.6, 7, 4, 9.6, 9),
+            Block.makeCuboidShape(2, 8.6, 6, 4, 9.6, 10),
+            Block.makeCuboidShape(1, 8.6, 7, 5, 9.6, 9)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+
+    private static final VoxelShape SHAPE_S = Stream.of(
+            Block.makeCuboidShape(7, 6.6, 0, 9, 7.6, 6),
+            Block.makeCuboidShape(7, 5.6, 6, 9, 6.6, 7),
+            Block.makeCuboidShape(7, 7.6, 2, 9, 9.6, 4),
+            Block.makeCuboidShape(6, 8.6, 2, 10, 9.6, 4),
+            Block.makeCuboidShape(7, 8.6, 1, 9, 9.6, 5)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+
+    private static final VoxelShape SHAPE_W = Stream.of(
+            Block.makeCuboidShape(10, 6.6, 7, 16, 7.6, 9),
+            Block.makeCuboidShape(9, 5.6, 7, 10, 6.6, 9),
+            Block.makeCuboidShape(12, 7.6, 7, 14, 9.6, 9),
+            Block.makeCuboidShape(12, 8.6, 6, 14, 9.6, 10),
+            Block.makeCuboidShape(11, 8.6, 7, 15, 9.6, 9)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+
+    private static final Map<Direction, VoxelShape> SHAPES = Maps.newEnumMap(ImmutableMap.of(
+            Direction.NORTH, SHAPE_N, Direction.EAST, SHAPE_E,
+            Direction.SOUTH, SHAPE_S, Direction.WEST, SHAPE_W
+    ));
 
     public TreeTapBlock() {
         super(Block.Properties.create(Material.WOOD)
@@ -42,10 +92,28 @@ public class TreeTapBlock extends Block {
         return true;
     }
 
-    @Nullable
     @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES.get(state.get(FACING));
+    }
+
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        return worldIn.getBlockState(pos.offset(state.get(FACING).getOpposite())).getMaterial().isSolid();
+    }
+
+    @Nullable
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+        BlockState blockstate = this.getDefaultState();
+        Direction[] adirection = context.getNearestLookingDirections();
+        for(Direction direction : adirection) {
+            if (direction.getAxis().isHorizontal()) {
+                Direction direction1 = direction.getOpposite();
+                return blockstate.with(FACING, direction1);
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -61,5 +129,10 @@ public class TreeTapBlock extends Block {
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return 1f;
     }
 }
